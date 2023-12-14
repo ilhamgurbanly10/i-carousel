aff4c3047da99300bc84ccaec22e6e926283deaf
ilhamgurbanly10 <Ilham_gurbanly@mail.ru>
2023-12-14 20:06:10 +0400

    progress-line

--- i-notification.js
+++ i-notification.js
@@ -1,6 +1,8 @@
-const iNotification = (type = 'success', title, mes, position = 'top-right', icon, time) => {
+const iNotification = (props) => {
 
-    const defaultTime = 3000;
+    const {type = 'success', title, mes, position = 'top-right', icon, time, progress = true} = props;
+
+    const defaultTime = 4000;
     const containerClasses = {
         'top-left': 'i-notification-container-top-left',
         'top-right': 'i-notification-container-top-right',
@@ -62,7 +64,7 @@ const iNotification = (type = 'success', title, mes, position = 'top-right', ico
         const _mes =  document.createElement('p');
         _mes.className = 'i-notification-mes';
         _mes.innerHTML = mes ? mes : types[type]?.mes;
-        body.appendChild(_mes);
+        if (mes) body.appendChild(_mes);
 
         const hide = () => {
             notification.classList.remove('i-notification-show');
@@ -77,9 +79,14 @@ const iNotification = (type = 'success', title, mes, position = 'top-right', ico
         close.addEventListener('click', hide)
         close.innerHTML = '<svg fill-rule="evenodd" viewBox="64 64 896 896" focusable="false" data-icon="close" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M799.86 166.31c.02 0 .04.02.08.06l57.69 57.7c.04.03.05.05.06.08a.12.12 0 010 .06c0 .03-.02.05-.06.09L569.93 512l287.7 287.7c.04.04.05.06.06.09a.12.12 0 010 .07c0 .02-.02.04-.06.08l-57.7 57.69c-.03.04-.05.05-.07.06a.12.12 0 01-.07 0c-.03 0-.05-.02-.09-.06L512 569.93l-287.7 287.7c-.04.04-.06.05-.09.06a.12.12 0 01-.07 0c-.02 0-.04-.02-.08-.06l-57.69-57.7c-.04-.03-.05-.05-.06-.07a.12.12 0 010-.07c0-.03.02-.05.06-.09L454.07 512l-287.7-287.7c-.04-.04-.05-.06-.06-.09a.12.12 0 010-.07c0-.02.02-.04.06-.08l57.7-57.69c.03-.04.05-.05.07-.06a.12.12 0 01.07 0c.03 0 .05.02.09.06L512 454.07l287.7-287.7c.04-.04.06-.05.09-.06a.12.12 0 01.07 0z"></path></svg>'
 
+        const _progress = document.createElement('div');
+        _progress.className = "i-notification-progress";
+        _progress.style.animationDuration = `${time / 1000}s`;
+
         notification.appendChild(head);
         notification.appendChild(body);
         notification.appendChild(close);
+        if (progress) notification.appendChild(_progress);
         
         setTimeout(() => {
             notification.classList.add('i-notification-show')
@@ -89,10 +96,12 @@ const iNotification = (type = 'success', title, mes, position = 'top-right', ico
         }, time ? time + 100 : defaultTime + 100);
 
         notification.addEventListener('mouseenter', () => {
+            notification.classList.add('i-notification-hovered');
             clearInterval(timeout);
         })
 
         notification.addEventListener('mouseleave', () => {
+            notification.classList.remove('i-notification-hovered');
             timeout = setTimeout(() => {
                 hide();
             }, time ? time + 100 : defaultTime + 100);
